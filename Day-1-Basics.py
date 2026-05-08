4ad926ac755eef0fa2c6237022bfec47c9f0e87e
Chinmayguptha <chinmaykumar2k4@gmail.com>
2026-05-08 18:59:07 +0530

    Day 01 - Learned variables and data types

---
+++ Day-1-Basics.py
@@ -0,0 +1,167 @@
+name = "yeahh"
+role = "ai-engineer"
+salary = 190000
+print(name)
+print(role)
+print(salary)
+
+
+
+price = 99.99
+height = 5.6
+
+
+print(price)
+print(height)
+print(type(price))
+print(type(height))
+
+
+name = "ckg"
+city = 'Bangalore'
+print(name)
+print(city)
+
+
+is_student = True
+is_logged_in = False
+
+
+age = 25
+
+print(type(age))
+
+
+name = "g"
+print(type(name))
+
+
+name = "c"
+age = 20
+
+print(name)
+print(age)
+
+
+
+name = "uttam"
+age = 18
+
+print(name, age)
+
+
+
+x = 10
+print(x)
+
+x = 20
+print(x)
+
+
+a, b, c = 1, 2, 3
+
+print(a)
+print(b)
+print(c)
+
+
+x = y = z = 100
+print(x)
+print(y)
+print(z)
+
+
+
+#name = input("Enter your name: ")
+
+#print(name)
+
+
+
+age = 25
+print(age)
+
+
+age = 30
+sal = 17
+print(age)
+print(sal)
+
+
+print(type(age))
+print(type(sal))
+
+
+msg = "hello"
+print(type(msg))
+
+msg2 = 'world'
+print(type(msg2))
+
+
+s1 = "hello"
+s2 = 'world'
+
+print(s1 + " " + s2)
+
+print("Hi" * 3)
+
+is_student = True
+is_logged_in = False
+print(is_student)
+print(type(is_logged_in))
+
+
+companies = ["Amazon" , "Boeing" , "Capgemini" , "Duetschese Bank"]
+print(companies)
+print(type(companies))
+print(companies[0])
+
+companies[1] = "BMW"
+print(companies[1])
+
+
+nums = (1, 2, 3)
+print(nums)
+print(type(nums))   
+print(nums[0])
+
+sett = {1, 2, 3}
+print(sett)
+print(type(sett))   
+
+ss1 = {1, 2, 3,3}
+print(ss1)
+
+student={
+    "name": "naam",
+    "age": 25,
+    "city": "Bangalore"
+}
+
+
+print(student)
+print(type(student))
+print(student["name"])
+
+
+x = None
+print(x)
+print(type(x))
+
+
+tenn = "10"
+t = int(tenn)
+print(t)
+f = float(tenn)
+print(f)
+print(type(tenn))
+print(type(t))
+print(type(f))
+
+
+age = 25
+print(str(age))
+
+age = input("Enter your age: ")
+print(type(age))
