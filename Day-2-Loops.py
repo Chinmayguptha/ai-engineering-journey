f2f2d9a5968d025071bf54e4cc5dda98d77c4f40
Chinmayguptha <chinmaykumar2k4@gmail.com>
2026-05-10 10:54:57 +0530

    Day 02 - Learned loops and password checker

---
+++ Day-2-Loops.py
@@ -0,0 +1,128 @@
+for i in range(3):
+    print("I'm an AI Engineer")
+
+for i in range(1,6):
+    print(i)
+
+for i in range(3):
+    print(i);
+
+
+for i in range(0,10,2):
+    print(i)
+
+role = "AI Engineer";
+for l in role:
+    print(l)
+
+
+
+mnc = [ "Google" , "Microsoft" , "Amazon" , "Facebook" , "Apple" ]
+
+for c in mnc:
+    print(c)
+
+
+
+
+x = 1
+
+while x <= 9:
+    print(x)
+    x = x + 1
+
+
+
+for i in range(6):
+    if i  == 3:
+        break
+    print(i)
+
+
+for i in range(6):
+    if i  == 5:
+        continue
+    print(i)
+
+
+
+# i dont know wt to do so i jst pass for now later i plan those things accordingly
+
+for i in range(100):
+    pass
+
+
+for i in range(5):
+    for j in range(3):
+        print(i,j)
+
+
+
+for i in range(4):
+    print("***")
+
+
+for i in range(1,6):
+    print("*" * i)
+
+
+
+for i in range(5):
+    print(i)
+else:
+    print("end d loop")
+
+
+
+#now let me write a code for the password attempts
+
+
+'''
+attempts = 3
+
+while attempts > 0:
+    pas = input("Enter the pass:")
+
+    if pas == "1a2b3c":
+        print("Login")
+        break
+    else:
+        print("Wrong pass")
+    
+    attempts -= 1
+'''
+
+
+
+sum = 0
+
+for i in range(1,6):
+    sum += i
+
+print(sum)
+
+
+
+comp = ["Ggl" , "o","mc","mt"]
+
+for i , c in enumerate(comp):
+    print(i,c)
+
+
+
+# num = 5
+
+# for i in range(1, 11):
+#     print(num, "x", i, "=", num * i)
+
+
+
+for i in range(10,0,-1):
+    print(i)
+
+
+
+
+
+
+    
