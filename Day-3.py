6e124c4ea6b016c4b585b7936e06730a6fe0eaea
Chinmayguptha <chinmaykumar2k4@gmail.com>
2026-05-10 11:28:48 +0530

    Day 03 - Learned functions in Python

---
+++ Day-3.py
@@ -0,0 +1,81 @@
+def greet():
+    print("Good Morning")
+
+
+greet()
+
+
+def g(name):
+    print("Hello" + "" + name)
+
+g("heyy")
+
+
+
+def add(a,b):
+    print(a+b)
+add(4,5)
+
+
+
+def sub(x,y):
+    return x - y
+
+
+
+res = sub(10,5)
+
+
+print(res)
+
+
+
+def sq(n):
+    return n * n
+
+soln = sq(5)
+print(soln)
+
+
+naam = "dell"
+print(len(naam))
+
+print(type(naam))
+
+
+
+def greet(name = "Guest"):
+    print("Hello" + "" + name)
+
+greet()
+
+greet("abc")
+
+
+def stu(nn,ag):
+    print(nn,ag)
+
+stu(nn = "xyz", ag = 20)
+
+
+
+def test():
+
+    x = 5
+    print(x)
+
+
+test()
+
+#print(x)
+
+
+namee = "mnop"
+
+def tt():
+    print(namee)
+
+tt()
+
+
+print(namee)
