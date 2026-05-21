4d4154b8d8707a37c157bfa9d152e9ac2953a3b6
Chinmayguptha <chinmaykumar2k4@gmail.com>
2026-05-21 19:35:21 +0530

    Day 10 - Completed matplotlib

--- Day-10-Matplotlib.py
+++ Day-10-Matplotlib.py
@@ -1,10 +1,33 @@
+#import matplotlib.pyplot as plt
+
+#x = [ 1 , 2 , 3 , 4 , 5 ]
+
+#y = [ 2 , 3 , 5 , 7 , 11]
+
+
+#plt.plot(x,y)
+
+#plt.show()
+
+
+
+
+#import matplotlib.pyplot as plt
+
+#x = [ 1 , 2 , 3 , 4 , 5 ]
+#y = [ 2 , 3 , 5 , 7 , 11 ]
+
+#marks = [ 86 , 98 , 98 , 98 , 100 , 99]
+#sub = [ "Maths" , "Science" , "English" , "Hindi" , "SST" , "Computer" ]
+
+#plt.hist(marks)
+#plt.show()
+
+
 import matplotlib.pyplot as plt
 
-x = [ 1 , 2 , 3 , 4 , 5 ]
+hrs = [ 2 , 3 , 4 , 5 , 6]
+marks = [64 , 71 , 77 , 85 , 93]
 
-y = [ 2 , 3 , 5 , 7 , 11]
-
-
-plt.plot(x,y)
-
+plt.scatter(hrs,marks)
 plt.show()
